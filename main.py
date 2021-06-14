e23b9b244a77e2fa63ef73d0aa59c61dcb5a0be0
zales2 <77017523+zales2@users.noreply.github.com>
2021-06-14 11:50:06 +0200

    commit

--- main.py
+++ main.py
@@ -4,9 +4,5 @@ app = create_app()
 
 if __name__ == '__main__':
     #app.run( debug = True )
-<<<<<<< HEAD
-    app.run( host = '0.0.0.0', port = 5000 )
-=======
     app.run( debug = True, port = 33507 )
->>>>>>> 59c79319eb2bf2b15ed786039f879acb17be0cef
 
