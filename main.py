da41793b1941e77c63329e726034a67fe17f465d
zales2 <77017523+zales2@users.noreply.github.com>
2021-06-15 02:32:29 +0200

    commit

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import os
 app = create_app()
 
 if __name__ == '__main__':
-    #app.run( debug = True )
     port = int( os.environ.get( 'PORT', 5000 ) )
     app.run( host='0.0.0.0', port = port )
 
